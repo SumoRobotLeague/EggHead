94a8840dabb7a7f106e049c4757889f318d6391a
Eric Ryan Harrison <eric@moonrise.works>
2018-03-25 13:59:21 -0500

    Print out our IR values for configuration purposes.

---
+++ SensorTest.ino
@@ -0,0 +1,37 @@
+/********************************************************************
+EggHead - Easter Egg Hunting Robot
+
+Author - Eric Ryan Harrison <me@ericharrison.info>
+http://github.com/SumoRobotLeague/EggJead/ contains the complete robot.
+
+Our code is open source, MIT License (details at the end).
+Please use our code in your projects and tell your friends about us!
+The MRK-2 robot kit is available from http://www.SumoRobotLeague.com
+********************************************************************/
+
+// IR Sensor pins
+#define leftSensor  A1
+#define rightSensor A2
+#define IREmitter   4
+
+void setup() {
+	Serial.being(9600);
+
+	// IR sensors
+	pinMode(leftSensor,  INPUT);
+	pinMode(rightSensor, INPUT);
+	pinMode(IREmitter,   OUTPUT);
+
+	// Turn on our IR Emitter
+	digitalWrite(IREmitter, HIGH);
+}
+
+void loop() {
+	int leftIR   = analogRead(leftSensor);
+	int rightIR  = analogRead(rightSensor);
+
+	Serial.print("Left: ");
+	Serial.print(leftIR);
+	Serial.print(", Right: ");
+	Serial.println(rightIR);
+}
